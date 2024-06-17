37be0e220bddc3d3e50ca861ab4e6bb6d225acea
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-06-18 00:11:38 +0700

    developer: re-order note data parameters

--- src/sampleData.tsx
+++ src/sampleData.tsx
@@ -2,27 +2,27 @@ import { NoteData } from "./components";
 
 export const sampleNotes: NoteData[] = [
   {
+    key: "k06p",
     text: "Buy\neggs, pork, pancake mix",
     color: 0,
-    rotate: -1.4,
-    key: "k06p",
     x: 173,
     y: 89,
+    rotate: -1.4,
   },
   {
+    key: "b4pl",
     text: "Dentist\n2024-01-20\n11:00",
     color: 6,
-    rotate: 1.7,
-    key: "b4pl",
     x: 279,
     y: 311,
+    rotate: 1.7,
   },
   {
+    key: "jo9f",
     text: "call Fred\n123-456-7809",
     color: 4,
-    rotate: -0.3,
-    key: "jo9f",
     x: 244,
     y: 527,
+    rotate: -0.3,
   },
 ];

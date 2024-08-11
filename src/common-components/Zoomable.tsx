7945056b81c4a2a4c58e646f4b0b924b4808e280
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-08-11 21:10:14 +0700

    new feature: support mobile pinch zoom

--- src/common-components/Zoomable.tsx
+++ src/common-components/Zoomable.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useState, WheelEvent } from "react";
 
 export function Zoomable({
   className,
@@ -33,16 +33,78 @@ export function Zoomable({
     return () => clearInterval(interval);
   }, [zoomTarget]);
 
+  const onWheel = ({ deltaY }: WheelEvent) => {
+    const newZoom = zoom - deltaY / 100 / 2;
+    const roundedZoom = Math.round(newZoom * 2) / 2;
+    setZoomTarget(roundedZoom);
+  };
+
+  const { onPointerDown } = usePinchZoom({ zoom, onZoom });
+
   return (
-    <div
-      className={className}
-      onWheel={({ deltaY }) => {
-        const newZoom = zoom - deltaY / 100 / 2;
-        const roundedZoom = Math.round(newZoom * 2) / 2;
-        return setZoomTarget(roundedZoom);
-      }}
-    >
+    <div className={className} onWheel={onWheel} onPointerDown={onPointerDown}>
       {children}
     </div>
   );
+}
+
+function usePinchZoom({ zoom, onZoom }: { zoom: number; onZoom: (zoom: number) => void }) {
+  const [pointers, setPointer] = useState<(PointerEvent | React.PointerEvent)[]>([]);
+  const [lastDist, setLastDist] = useState(0);
+
+  useEffect(() => {
+    if (pointers.length > 0) {
+      document.addEventListener("pointermove", onPointerMove);
+      document.addEventListener("pointerup", onPointerUp);
+      document.addEventListener("pointercancel", onPointerUp);
+      document.addEventListener("pointerout", onPointerUp);
+      document.addEventListener("pointerleave", onPointerUp);
+
+      return () => {
+        document.removeEventListener("pointermove", onPointerMove);
+        document.removeEventListener("pointerup", onPointerUp);
+        document.removeEventListener("pointercancel", onPointerUp);
+        document.removeEventListener("pointerout", onPointerUp);
+        document.removeEventListener("pointerleave", onPointerUp);
+      };
+    }
+  }, [pointers, onPointerMove, onPointerUp]);
+
+  function onPointerDown(e: React.PointerEvent) {
+    const updatedPointers = [...pointers, e];
+
+    if (updatedPointers.length === 2) {
+      const dx = Math.abs(updatedPointers[1].clientX - updatedPointers[0].clientX);
+      const dy = Math.abs(updatedPointers[1].clientY - updatedPointers[0].clientY);
+      const pointerDist = Math.max(dx, dy);
+      setLastDist(pointerDist);
+    }
+
+    setPointer(updatedPointers);
+  }
+
+  function onPointerMove(e: PointerEvent) {
+    const updatedPointers = [...pointers.filter(({ pointerId }) => pointerId !== e.pointerId), e];
+
+    if (updatedPointers.length === 2) {
+      const dx = Math.abs(updatedPointers[1].clientX - updatedPointers[0].clientX);
+      const dy = Math.abs(updatedPointers[1].clientY - updatedPointers[0].clientY);
+      const pointerDist = Math.max(dx, dy);
+
+      const distDiff = pointerDist / (lastDist || pointerDist);
+      const newZoom = Math.log(2 ** zoom * distDiff) / Math.log(2);
+      onZoom(newZoom);
+
+      setLastDist(pointerDist);
+    }
+
+    setPointer(updatedPointers);
+  }
+
+  function onPointerUp(e: PointerEvent) {
+    const updatedPointers = pointers.filter(({ pointerId }) => pointerId !== e.pointerId);
+    setPointer(updatedPointers);
+  }
+
+  return { onPointerDown };
 }
